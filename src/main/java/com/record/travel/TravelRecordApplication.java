5236781731ec01dcbc8d93dc2269ab205d9b64c9
yejin <dpwls0802@naver.com>
2022-01-15 21:22:51 +0900

    first setting

---
+++ src/main/java/com/record/travel/TravelRecordApplication.java
@@ -0,0 +1,13 @@
+package com.record.travel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelRecordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/record/travel/TravelRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.record.travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
