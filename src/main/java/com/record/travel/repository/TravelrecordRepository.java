fd3ca59629ee851c8081a2b4ee4a4876334cc8b4
yejin <dpwls0802@naver.com>
2022-01-20 01:11:28 +0900

    CRUD test

--- src/main/java/com/record/travel/repository/TravelrecordRepository.java
+++ src/main/java/com/record/travel/repository/TravelrecordRepository.java
@@ -1,10 +1,9 @@
 package com.record.travel.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import com.record.travel.entity.Travelrecord;
 
-public interface TravelrecordRepository extends JpaRepository<Travelrecord, Long>, QuerydslPredicateExecutor<Travelrecord>{
+public interface TravelrecordRepository extends JpaRepository<Travelrecord, Long>{
 
 }
--- src/test/java/com/record/travel/repository/TravelrecordRepositoryTests.java
+++ src/test/java/com/record/travel/repository/TravelrecordRepositoryTests.java
@@ -1,5 +1,6 @@
 package com.record.travel.repository;
 
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.Test;
@@ -13,13 +14,48 @@ public class TravelrecordRepositoryTests {
 	@Autowired
 	private TravelrecordRepository travelrecordRepository;
 
+	// 등록
 	@Test
 	public void insert() {
 		IntStream.rangeClosed(1, 100).forEach(i -> {
 			Travelrecord travelrecord = Travelrecord.builder().title(i + "번째 제목").content(i + "번째 내용")
-					.writer((i % 10) + "번째 작성자").travelDate("2021-01-"+ (i%10) + " ~ 2021-02-" + (i%10)).build();
+					.writer((i % 10) + "번째 작성자").travelDate("2021-01-" + (i % 10) + " ~ 2021-02-0" + (i % 10)).build();
 			System.out.println(travelrecordRepository.save(travelrecord));
 		});
+	}
+
+	// 조회
+	// @Test
+	public void select() {
+		Long tnum = 400L;
+		Optional<Travelrecord> result = travelrecordRepository.findById(tnum);
+
+		if (result.isPresent()) {
+			Travelrecord travelrecord = result.get();
+			System.out.println(travelrecord);
+		}
+	}
+
+	// 수정
+	//@Test
+	public void update() {
+		Travelrecord travelrecord = Travelrecord.builder().tnum(400L).title("제목 변경").content("내용 변경").writer("작성자 변경")
+				.travelDate("여행날짜 변경").build();
+		System.out.println(travelrecordRepository.save(travelrecord));
+
+	}
+
+	// 삭제
+	// @Test
+	public void delete() {
+		Long tnum = 400L;
+		travelrecordRepository.deleteById(tnum);
+	}
+
+	// 전체 삭제
+	//@Test
+	public void deleteAll() {
+		travelrecordRepository.deleteAll();
 	}
 
 }
