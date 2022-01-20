524988706691cd0f578a63fed7459c80130177c2
yejin <dpwls0802@naver.com>
2022-01-21 00:04:10 +0900

    dtoToEntity

--- src/main/java/com/record/travel/service/TravelrecordService.java
+++ src/main/java/com/record/travel/service/TravelrecordService.java
@@ -5,7 +5,9 @@ import com.record.travel.entity.Travelrecord;
 
 public interface TravelrecordService {
 	Long register(TravelrecordDTO dto);
-
+	
+	//서비스 계층에서 파라미터를 DTO 타입으로 받기 때문에 JPA로 처리하기 위해 엔티티 타입의 객체로 변환해야 함
+	//DTO -> entity로 변환
 	default Travelrecord dtoToEntity(TravelrecordDTO dto) {
 		Travelrecord entity = Travelrecord.builder().tnum(dto.getTnum()).title(dto.getTitle()).content(dto.getContent())
 				.writer(dto.getWriter()).travelDate(dto.getTravelDate()).build();
--- src/main/java/com/record/travel/service/TravelrecordServiceImpl.java
+++ src/main/java/com/record/travel/service/TravelrecordServiceImpl.java
@@ -3,6 +3,7 @@ package com.record.travel.service;
 import org.springframework.stereotype.Service;
 
 import com.record.travel.dto.TravelrecordDTO;
+import com.record.travel.entity.Travelrecord;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -12,6 +13,12 @@ public class TravelrecordServiceImpl implements TravelrecordService {
 	
 	@Override
 	public Long register(TravelrecordDTO dto) {
+		log.info("===============");
+		log.info(dto);
+		
+		Travelrecord entity = dtoToEntity(dto);
+		log.info(entity);
+		
 		return null;
 	}
 }
