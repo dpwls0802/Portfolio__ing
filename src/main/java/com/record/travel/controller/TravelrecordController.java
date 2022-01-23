4de6bae912a5648fae8aa005e75a8e5f63e0c6bc
yejin <dpwls0802@naver.com>
2022-01-24 00:53:36 +0900

    list controller, view, paging

--- src/main/java/com/record/travel/controller/TravelrecordController.java
+++ src/main/java/com/record/travel/controller/TravelrecordController.java
@@ -1,21 +1,29 @@
 package com.record.travel.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.record.travel.dto.PageRequestDTO;
+import com.record.travel.service.TravelrecordService;
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Controller
 @RequestMapping("/travelrecord")
 @Log4j2
+@RequiredArgsConstructor
 public class TravelrecordController {
+	
+	private final TravelrecordService service;
 	
 	//목록
 	@GetMapping("/list")
-	public String list() {
-		log.info("=============list=========");
-		return "/travelrecord/list";
+	public void list(PageRequestDTO pageRequestDTO, Model model) {
+		log.info("=============list=========" + pageRequestDTO);
+		model.addAttribute("result", service.getList(pageRequestDTO));
 	}
 	
 	//등록
