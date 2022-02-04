36fca29ad1dfccca4fefcc925fce09984f405767
yejin <dpwls0802@naver.com>
2022-02-04 22:55:37 +0900

    reply list, read. remove finish / modify ing

--- src/main/java/com/record/travel/controller/ReplyController.java
+++ src/main/java/com/record/travel/controller/ReplyController.java
@@ -5,8 +5,12 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,6 +36,35 @@ public class ReplyController {
 		log.info("tnum : " + tnum);
 		
 		return new ResponseEntity<>(replyService.getList(tnum), HttpStatus.OK);
+	}
+	
+	//등록
+	@PostMapping("")
+	public ResponseEntity<Long> register(@RequestBody ReplyDTO replyDTO) {
+		log.info(replyDTO);
+		
+		Long rnum = replyService.register(replyDTO);
+		
+		return new ResponseEntity<>(rnum, HttpStatus.OK);
+	}
+	
+	//삭제
+	@DeleteMapping("/{rnum}")
+	public ResponseEntity<String> remove(@PathVariable("rnum") Long rnum) {
+		log.info("댓글 번호 :" + rnum);
+		
+		replyService.remove(rnum);
+		return new ResponseEntity<>("success", HttpStatus.OK);
+	}
+	
+	//수정
+	@PutMapping("/{rnum}")
+	public ResponseEntity<String> modify(@RequestBody ReplyDTO replyDTO) {
+		log.info(replyDTO);
+		
+		replyService.modify(replyDTO);
+		
+		return new ResponseEntity<>("success", HttpStatus.OK);
 	}
 	
 }
